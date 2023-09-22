5edb0555c850aee786e7f18a6f71606842204799
LAU ZHONG REN <92208723+AaronLauZR@users.noreply.github.com>
2023-09-22 11:43:58 +0800

    update

--- app.py
+++ app.py
@@ -76,7 +76,24 @@ def submit_form():
     # Render the form page. You can also return a template if you have one.
     return render_template('SubmitInternshipForm.html')  # Replace with your template name
 
-
+@app.route('/', methods=['POST'])
+def create_user():
+    name = request.form['name']
+    password = request.form['password']
+    email = request.form['email']
+    role = request.form['role']
+    
+    # You can now use the 'name', 'password', 'email', and 'role' variables as needed.
+    
+    # For example, you can print them:
+    print(f"Name: {name}")
+    print(f"Password: {password}")
+    print(f"Email: {email}")
+    print(f"Role: {role}")
+    
+    # You can also perform further actions with these values here.
+    
+    return "User created successfully"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
